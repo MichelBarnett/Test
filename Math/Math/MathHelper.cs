154aa5b6a5547aef1a686a77a57727fb647f3f17
Michel Barnett <michelb@microsoft.com>
2017-06-29 08:18:19 -0700

    initial implementation

---
+++ Math/Math/MathHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math
+{
+    public class MathHelper
+    {
+        private MathHelper() { }
+
+        public static int Add(int param1, int param2)
+        {
+            int sum = param1 + param2;
+            return sum;
+        }
+
+        public static int Subtract(int param1, int param2)
+        {
+            int difference = param1 - param2;
+            return difference;
+        }
+    }
+}
---
+++ Math/Math/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Math Sample");
+            Console.WriteLine();
+
+            int param1 = 4;
+            int param2 = 8;
+            int sum = MathHelper.Add(param1, param2);
+            Console.WriteLine($"{param1} + {param2} = {sum}");
+            Console.WriteLine();
+
+            param1 = 15;
+            param2 = 4;
+            int difference = MathHelper.Subtract(param1, param2);
+            Console.WriteLine($"{param1} - {param2} = {difference}");
+            Console.WriteLine();
+
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
+        }
+    }
+}
